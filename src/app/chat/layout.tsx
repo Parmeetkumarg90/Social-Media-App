e1153eb00cc840ff336b1fa01d230eaf4e546904
Parmeet Kumar <parmeetkumarg90.com>
2025-10-24 18:25:40 +0530

    fix promise in params

--- src/app/chat/layout.tsx
+++ src/app/chat/layout.tsx
@@ -4,19 +4,45 @@ import style from "./style.module.css";
 import Card from "@mui/material/Card";
 import UserList from "@/components/list/user-list-chat/userList";
 import Navbar from "@/components/dashboard/nav-bar/navbar";
+import { useEffect, useState } from "react";
+import { redirect } from "next/navigation";
 
 const ChatLayout = ({ children, params }: Readonly<{
     children: React.ReactNode,
-    params: {
-        id: string
-    }
+    params: Promise<{ id: string }>
 }>) => {
+    const [id, setId] = useState<{ id: string } | null>(null);
+
+    useEffect(() => {
+        getId();
+    }, []);
+
+    const getId = async () => {
+        try {
+            const param = await params;
+            if (param.id) {
+                setId(param);
+            }
+            else {
+                redirect("/");
+            }
+        }
+        catch (e) {
+            console.log("Invalid id: ", e);
+            redirect("/");
+        }
+    }
+
     return (
         <Card className={`${style.main_grid}`}>
             <Navbar />
             <Card className={`${style.grid}`}>
-                <UserList params={params} />
-                {children}
+                {id &&
+                    <>
+                        <UserList params={id} />
+                        {children}
+                    </>
+                }
             </Card>
         </Card>
     )
--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,5 +1,4 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getDatabase } from 'firebase/database';
 import { getAuth } from "firebase/auth";
 import { GoogleAuthProvider } from "firebase/auth";
@@ -17,7 +16,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const firbaseRealDb = getDatabase(app);
 const firestoreDb = getFirestore(app);
 const auth = getAuth(app);
