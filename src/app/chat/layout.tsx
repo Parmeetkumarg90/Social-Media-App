8aeeffdf1acdd179c81f419fb2279b756f001e8b
Parmeet Kumar <parmeetkumarg90.com>
2025-10-24 18:33:04 +0530

    fix promise in params

--- src/app/chat/layout.tsx
+++ src/app/chat/layout.tsx
@@ -4,45 +4,18 @@ import style from "./style.module.css";
 import Card from "@mui/material/Card";
 import UserList from "@/components/list/user-list-chat/userList";
 import Navbar from "@/components/dashboard/nav-bar/navbar";
-import { useEffect, useState } from "react";
-import { redirect } from "next/navigation";
+import { useAppSelector } from "@/redux/hook";
+import { RootState } from "@/redux/store";
 
-const ChatLayout = ({ children, params }: Readonly<{
-    children: React.ReactNode,
-    params: Promise<{ id: string }>
-}>) => {
-    const [id, setId] = useState<{ id: string } | null>(null);
-
-    useEffect(() => {
-        getId();
-    }, []);
-
-    const getId = async () => {
-        try {
-            const param = await params;
-            if (param.id) {
-                setId(param);
-            }
-            else {
-                redirect("/");
-            }
-        }
-        catch (e) {
-            console.log("Invalid id: ", e);
-            redirect("/");
-        }
-    }
+const ChatLayout = ({ children }: Readonly<{ children: React.ReactNode, }>) => {
+    const loggedInUser = useAppSelector((state: RootState) => state.currentUser);
 
     return (
         <Card className={`${style.main_grid}`}>
             <Navbar />
             <Card className={`${style.grid}`}>
-                {id &&
-                    <>
-                        <UserList params={id} />
-                        {children}
-                    </>
-                }
+                <UserList params={{ id: loggedInUser.id }} />
+                {children}
             </Card>
         </Card>
     )
