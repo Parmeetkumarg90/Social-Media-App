1232effd610dd2f599addd676d0563690182b148
Parmeet Kumar <parmeetkumarg90.com>
2025-10-24 18:03:23 +0530

    remove eslint error of no-explicit-any

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -17,7 +17,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+const analytics = getAnalytics(app);
 const firbaseRealDb = getDatabase(app);
 const firestoreDb = getFirestore(app);
 const auth = getAuth(app);
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-import { cookies, headers } from 'next/headers';
+import { cookies } from 'next/headers';
 
 const publicRoutes = ['/', '/register', '/path:*.svg'];
 
